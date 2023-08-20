c6ad8713aabe54181e795da784c478b6f30abeff
Howard <howardtuan@gmail.com>
2023-08-20 17:37:31 +0800

    8/20ppt
    ppt半成品
    這在研究如何切換func

--- code_test/pptMode_new.py
+++ code_test/pptMode_new.py
@@ -8,72 +8,18 @@ with open('secret.json', 'r') as file:
 OPENAI_API_KEY = data.get("OPENAI_API_KEY")
 app = Flask(__name__)
 static_folder='static'
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route("/get_dialogue_summary", methods=["POST"])
-def get_dialogue_summary():
-    openai.api_key = OPENAI_API_KEY
-    input_param = request.get_json().get('inputParam')
-    print('接收的逐字稿',input_param)
-    completion = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo",
-      temperature=0,   
-      messages=[
-            {"role": "system", "content": "你是一位負責將逐字稿轉換為重點整理的工作人員，請將以下逐字稿修改為大綱以及重點整理，格式為「主題：」「大綱：」「重點整理：」請條列式自動換行:"},
-            {"role": "user", "content": input_param}
-        ]
-    )
-    print(completion.choices[0].message.content)
-
-    completed_text = completion.choices[0].message.content
-    print(completed_text)
-    return jsonify(response=completed_text)
-
-@app.route("/get_sentence_analyze", methods=["POST"])
-def get_sentence_analyze():
-    openai.api_key = OPENAI_API_KEY
-    input_param = request.get_json().get('inputParam')
-    print('接收的逐字稿',input_param)
-    completion = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo-16k",
-      messages=[
-            #進入特殊模式後，需要另外寫進入該模式的邏輯
-            {"role": "system", "content": "你是負責判斷句子中重點含義的人，需要注意的點有以下：當語意是「用AI繪圖」，則回應「AI生圖」；當語意是「自己拍的照片」，則回應「相簿取圖」；當語意是「幫我查詢...或是該句子可以透過網路得到答案」，則回應「爬蟲模式」；當語意是「使用簡報」，則回應「簡報模式」；當語意是「產生圖表」，則回應「圖表模式」；若語意沒有以上的內容，則回應「None」，不可以有其他類型的回應，僅可以回應以上我允許的文字"},
-            {"role": "user", "content": "欸你看我昨天在日月潭拍的夕陽"},
-            {"role": "assistant", "content": "相簿取圖"},
-            {"role": "user", "content": "幫我用AI產生有一隻狗在草原上奔跑的圖"},
-            {"role": "assistant", "content": "AI生圖"},
-            {"role": "user", "content": "進入簡報模式"},
-            {"role": "assistant", "content": "簡報模式"},
-            {"role": "user", "content": "進入圖表模式，五月的銷售額為20萬，六月的有90萬"},
-            {"role": "assistant", "content": "圖表模式"},
-            {"role": "user", "content": "好想睡覺"},
-            {"role": "assistant", "content": "None"},
-            {"role": "user", "content": "幫我查麥當勞優惠的新聞"},
-            {"role": "assistant", "content": "爬蟲模式"},
-            {"role": "user", "content": input_param}
-        ]
-    )
-    print(completion.choices[0].message.content)
-
-    completed_text = completion.choices[0].message.content
-    print(completed_text)
-    return jsonify(response=completed_text)
-    # return render_template('index.html', completed_text=completed_text)  # 将 completed_text 传递给模板
-
+#  {"role": "assistant", "content": "{'Title':'','LittleTitle':[],'Content':[],'PIC':[]}"},
 initMessage="""
-    你是一個簡報小幫手，使用者會輸入幾句話，你要負責生成那幾句話的PPT標題、內容文字以及圖片（每次都盡量根據使用者輸入的文字去選擇適合的圖片，提供圖片名稱即可），請用python的字典格式{'Title':'','LittleTitle':[],'Content':[],'PIC':[]}來回答。LittleTitle跟Content及PIC的value在同一個index情況下會有對應的關係！
-    LittleTitle指的是這段文字的小標題。
-    Content是重點整理使用者輸入的內容，並將其精簡化，不可使文字跟輸入的文字一模一樣!
-    PIC通常會跟LittleTitle一樣，是輔助它的圖片！
-    當還沒進到下一頁時，若先前已經介紹過某個LittleTitle並加入至LittleTitle、Content、PIC的話，不可以移除已經加入的內容！但如果兩句話都在講同一個小主題的話，可以更新並重整該主題的Content，使句字更順暢！接收到「下一頁」時，就回傳清空value的字典！
+你是一個簡報小幫手，使用者會輸入幾句話，你要負責生成那幾句話的PPT標題、內容文字以及圖片（每次都盡量根據使用者輸入的文字去選擇適合的圖片，提供圖片名稱即可），請用python的字典格式{'Title':'','LittleTitle':[],'Content':[],'PIC':[]}來回答。LittleTitle跟Content及PIC的value在同一個index情況下會有對應的關係！
+LittleTitle指的是這段文字的小標題。
+Content是重點整理使用者輸入的內容，並將其精簡化，不可使文字跟輸入的文字一模一樣!
+PIC通常會跟LittleTitle一樣，是輔助它的圖片！
+當還沒進到下一頁時，若先前已經介紹過某個LittleTitle並加入至LittleTitle、Content、PIC的話，不可以移除已經加入的內容！但如果兩句話都在講同一個小主題的話，可以更新並重整該主題的Content，使句字更順暢！接收到「下一頁」時，就回傳清空value的字典！
 
-    """
-messages_db = [
+"""
+
+openai.api_key = OPENAI_API_KEY
+messages = [
     {"role": "system", "content": initMessage},
     {"role": "user", "content": "今天我要介紹的主題就是2023中原資管小琉球之旅"},
     {"role": "assistant", "content": "{'Title':'中原資管－小琉球之旅','LittleTitle':[],'Content':[],'PIC':['小琉球']}"},
@@ -110,45 +56,23 @@ messages_db = [
     {"role": "user", "content": "最後則是連在地人都激推的美食，小蝌蚪起司餅！酥脆的餅皮搭配香濃罪惡的起司和店家特調的醬料及海苔粉，鹹甜的口味越嚼越香，而且每一口都能吃到飛魚卵在嘴巴逼逼波波的滋味，超級幸福～"},
     {"role": "assistant", "content": "{'Title':'美食推薦','LittleTitle':['海龜燒','來這吃冰吧','小蝌蚪起司餅'],'Content':['海龜燒是海龜造型的雞蛋糕，外皮酥脆，充滿雞蛋和奶香，外觀可愛，味道甜美。「來這吃冰吧」是網美們喜愛的韓系風格雪花冰店，店內以白色簡約裝潢為主，帶有小清新的氛圍。最後，我們來介紹當地人大力推薦的美食，小蝌蚪起司餅！餅皮酥脆，搭配濃郁的起司、店家特調的醬料和海苔粉，鹹甜的口味令人愛不釋口。每一口都帶來飛魚卵爆漿的美味，極具幸福感。'],'PIC':['海龜燒','來這吃冰吧','小蝌蚪起司餅']}"},
     {"role": "user", "content": "以上是某一個主題的訓練範本，接下來的主題會截然不同，請記得以上的回應模式，但不要將主題內容跟後面的主題混為一談"},
-]   
+]
 
-@app.route("/get_ppt", methods=["POST"])
-def get_ppt():
-    openai.api_key = OPENAI_API_KEY
+while True:
+    user_input = input("You: ")
     
-    status=1
-    while status:
-        input_param = request.get_json().get('inputParam')
-        print('接收的逐字稿',input_param)
-        if "結束簡報模式" in input_param:
-            status=0
-            return jsonify(response='None')
-        
-        # 添加使用者輸入到對話歷史中
-        messages_db.append({"role": "user", "content": input_param})
-        
-        # 使用完整的對話歷史呼叫模型
-        completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo-16k",
-            messages=messages_db
-        )
-        
-        # 提取助手回應並輸出
-        assistant_response = completion.choices[0].message['content']
-        # 添加助手回應到對話歷史中
-        messages_db.append({"role": "assistant", "content": assistant_response})
-        print(completion.choices[0].message.content)
-
-        completed_text = completion.choices[0].message.content
-        print(completed_text)
-        return jsonify(response=completed_text)
-        
-        
-
-
+    # 添加使用者輸入到對話歷史中
+    messages.append({"role": "user", "content": user_input})
     
+    # 使用完整的對話歷史呼叫模型
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-16k",
+        messages=messages
+    )
     
-
-if __name__ == '__main__':
-    app.run()
-
+    # 提取助手回應並輸出
+    assistant_response = completion.choices[0].message['content']
+    print("AI:", assistant_response)
+    
+    # 添加助手回應到對話歷史中
+    messages.append({"role": "assistant", "content": assistant_response})
--- code_test/turbo_model.py
+++ code_test/turbo_model.py
@@ -11,3 +11,30 @@ while True:
         ]
     )
     print(completion.choices[0].message.content)
+
+# 循環呼叫版本
+# import openai
+
+# openai.api_key = 'api key'
+# messages = [
+#     {"role": "system", "content": "系統訊息，目前無用"},
+# ]
+
+# while True:
+#     user_input = input("You: ")
+    
+#     # 添加使用者輸入到對話歷史中
+#     messages.append({"role": "user", "content": user_input})
+    
+#     # 使用完整的對話歷史呼叫模型
+#     completion = openai.ChatCompletion.create(
+#         model="gpt-3.5-turbo",
+#         messages=messages
+#     )
+    
+#     # 提取助手回應並輸出
+#     assistant_response = completion.choices[0].message['content']
+#     print("AI:", assistant_response)
+    
+#     # 添加助手回應到對話歷史中
+#     messages.append({"role": "assistant", "content": assistant_response})
